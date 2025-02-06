bea253847c7e29de79fdf5e534952a7623f09cb2
Miguel <migueltony9@gmail.com>
2025-02-06 08:59:45 -0600

    Modal comentarios guarda valores

--- src/components/ComentarioModal.tsx
+++ src/components/ComentarioModal.tsx
@@ -1,5 +1,5 @@
 import '../styles/ComentarioModal.css'
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import iconSubir from '/public/img/icon-subir.png'
 
 interface comentarioModalProps{
@@ -7,6 +7,12 @@ interface comentarioModalProps{
   setModalComentario: (cancelar: boolean) => void;
 }
 
+interface formDataValues{
+  imagen: File | null;
+  rating: number;
+  titulo: string;
+  texto: string;
+}
 
 const ComentarioModal: React.FC<comentarioModalProps> = ({modalComentario, setModalComentario}) => {
 
@@ -15,7 +21,63 @@ const ComentarioModal: React.FC<comentarioModalProps> = ({modalComentario, setMo
     document.body.style.overflow = 'auto'
   }
 
+  // visualizar imagen en el input
+  const [previewImage, setPreviewImage] = useState<string | null>(null)
+  const [errorImage, setErrorImage] = useState<string | null>(null)
 
+  const handleChangeImage = (e:ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file){
+
+      const extensionesPermitidas = ['image/jpeg','image/png','image/jpg'];
+
+      if(!extensionesPermitidas.includes(file.type)){
+        setErrorImage('Formato no valido. Solo se permiten imágenes JPG, JPEG o PNG');
+        setPreviewImage(null);
+        return;
+      }
+
+      setErrorImage(null); //Si el formato es valido
+      const image = URL.createObjectURL(file);
+      setPreviewImage(image);
+      setFormData((prevData) => ({
+        ...prevData,
+        imagen: file,
+      }));
+    }
+  }
+
+  const quitarImagen = () => {
+    setPreviewImage(null)
+    setErrorImage(null)
+    setFormData((prevData) => ({
+      ...prevData,
+      imagen: null,
+    }));
+  }
+
+  const [formData, setFormData] = useState<formDataValues>({
+    imagen: null,
+    rating: 0,
+    titulo: '',
+    texto: ''
+  })
+  console.log(formData.imagen)
+
+  // Guardar datos del formulario 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({...formData,
+      [e.target.name]:[e.target.value]
+    })
+  }
+
+  const handleRatingChange = (value: number) => {
+    setFormData({
+      ...formData,
+      rating: value,
+    });
+  }
 
   return (
     <>
@@ -24,16 +86,24 @@ const ComentarioModal: React.FC<comentarioModalProps> = ({modalComentario, setMo
           <form action="" className='comentarioModalForm'>
             <label htmlFor="">Imagen</label>
             <div className='inputFile'>
-              <img src={iconSubir} alt="" />
-              <p>Subir imagen</p>
-              <input type="file" /> 
+              <input type="file" onChange={handleChangeImage}/> 
+              {previewImage ?
+              <>
+                <img src={previewImage} className='imgPreview' alt="" /> 
+                <button className='btnQuitImage' onClick={quitarImagen}>X</button>
+              </> :
+              <>
+                <img src={iconSubir} alt="" className='iconSubirImage' />
+                <p>Subir imagen</p>             
+              </>}
+               {errorImage && <p className='errorText'>{errorImage}</p>}
             </div>
 
             <label htmlFor="">Calificación</label>
             <div className='inputRadio' >
             {[...Array(5)].map((_,index)=>(
               <React.Fragment key={index}>
-                <input type="radio" name='rating' id={`start${5 - index}`} value={5 - index} />   
+                <input type="radio" name='rating' id={`start${5 - index}`} value={5 - index} onChange={()=>handleRatingChange(5 - index)}/>   
                 <label htmlFor={`start${5 - index}`} >★</label>
               </React.Fragment>              
             ))}
@@ -41,11 +111,11 @@ const ComentarioModal: React.FC<comentarioModalProps> = ({modalComentario, setMo
             
             
             <label htmlFor="">Titulo</label>
-            <input type="text" />
+            <input type="text" name='titulo' value={formData.titulo} onChange={handleChange}/>
 
 
             <label htmlFor="">Texto</label>
-            <input type="text" />
+            <input type="text" name='texto' value={formData.texto} onChange={handleChange} />
 
             <div className='botonesComentarioModal'>
               <button type='reset' onClick={cerrarModalComentario}>Cancelar</button>
